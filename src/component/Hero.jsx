51dd12747d86b9abc86fdd0ea7d1c6f4d9d72119
vikashbaheliya <vikasbaheliya95@gmail.com>
2022-09-15 00:33:42 +0530

    basic UI setup

---
+++ src/component/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header';
+
+
+function Hero(){
+    return(
+        <div style={{backgroundColor:'#f5ba13'}}>
+            <Header/>
+        </div>
+    )
+}
+
+export default Hero
